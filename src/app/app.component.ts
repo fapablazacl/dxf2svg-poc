29420cdcc4e67058ed1dd6f9e9e9c88325b2a4ce
Felipe Apablaza <felipe.apablaza@protonmail.com>
2022-12-09 08:52:00 -0300

    prueba

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DxfViewer } from "dxf-viewer"
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'dxf2svg-poc';
+
+  ngOnInit() {
+    DxfViewer.SetupWorker()
+  }
 }
